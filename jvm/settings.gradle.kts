a7b50d52afe60bcef853c088db997184a5aa0fc8
Loïc Lamarque <loiclamarque777@gmail.com>
2024-09-18 08:31:39 +0200

    💚 Fix Gradle dry-runs for `jvm` included build
    Workaround from https://github.com/gradle/gradle/issues/2517#issuecomment-437490287.

--- jvm/settings.gradle.kts
+++ jvm/settings.gradle.kts
@@ -4,3 +4,5 @@ dependencyResolutionManagement.versionCatalogs.register("libs") {
     val file: ConfigurableFileCollection = files("../gradle/libs.versions.toml")
     from(file)
 }
+
+gradle.startParameter.isDryRun = gradle.parent?.startParameter?.isDryRun == true
