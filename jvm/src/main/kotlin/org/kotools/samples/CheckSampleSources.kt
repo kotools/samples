e958ad3d363d01e4571b6c3eb51570edc2edab5d
Loïc Lamarque <loiclamarque777@gmail.com>
2025-05-27 21:34:02 +0200

    📝 Update documentation of `CheckSampleSources` type

--- jvm/src/main/kotlin/org/kotools/samples/CheckSampleSources.kt
+++ jvm/src/main/kotlin/org/kotools/samples/CheckSampleSources.kt
@@ -9,10 +9,20 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.work.DisableCachingByDefault
 import org.kotools.samples.internal.SampleSourceFile
 
-/** Task responsible for checking the content of sample sources. */
+/**
+ * Represents a Gradle task that checks the content of the sample source files
+ * present in the specified [source directory][sourceDirectory].
+ *
+ * A sample source file is a Kotlin or Java file that has a name suffixed by
+ * `Sample`.
+ *
+ * This task throws an [IllegalStateException] if a sample source file has an
+ * invalid content. For example, the sample source file's content is invalid if
+ * it doesn't contain a single public class declaration.
+ */
 @DisableCachingByDefault(because = "Only reading files doesn't worth caching.")
 public abstract class CheckSampleSources : DefaultTask() {
-    /** The directory containing sample sources. */
+    /** The directory containing the sample source files to check. */
     @get:InputDirectory
     @get:PathSensitive(PathSensitivity.NONE)
     public abstract val sourceDirectory: DirectoryProperty
