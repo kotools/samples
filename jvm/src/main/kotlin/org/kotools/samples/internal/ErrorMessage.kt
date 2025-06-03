35a3c9e1f30ebbac814c5bf9cd1bfa0dce725724
Loïc Lamarque <loiclamarque777@gmail.com>
2025-06-03 07:30:53 +0200

    ✨ Add `ErrorMessage` type

---
+++ jvm/src/main/kotlin/org/kotools/samples/internal/ErrorMessage.kt
@@ -0,0 +1,31 @@
+package org.kotools.samples.internal
+
+import java.io.File
+
+/**
+ * Represents an error message.
+ *
+ * For creating an instance of this type, see the factory functions provided by
+ * the [ErrorMessage.Companion] type.
+ */
+internal class ErrorMessage private constructor(private val text: String) {
+    /** Returns the string representation of this error message. */
+    override fun toString(): String = this.text
+
+    /** Contains static declarations for the [ErrorMessage] type. */
+    companion object {
+        /**
+         * Returns an error message indicating that multiple classes were found
+         * in the specified [file].
+         */
+        fun multipleClassesFoundIn(file: File): ErrorMessage =
+            ErrorMessage("Multiple classes found in '$file'.")
+
+        /**
+         * Returns an error message indicating that no public class was found in
+         * the specified [file].
+         */
+        fun noPublicClassFoundIn(file: File): ErrorMessage =
+            ErrorMessage("No public class found in '$file'.")
+    }
+}
