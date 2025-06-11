ceed58c9378c2d1925d2b87e0c4830bfe068e21d
Loïc Lamarque <loiclamarque777@gmail.com>
2025-06-12 00:36:21 +0200

    ✨ Improve `CheckSampleSources` Gradle task type
    This task now logs all errors encountered in sample source files instead of logging the first error.

--- jvm/src/main/kotlin/org/kotools/samples/CheckSampleSources.kt
+++ jvm/src/main/kotlin/org/kotools/samples/CheckSampleSources.kt
@@ -7,7 +7,13 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.work.DisableCachingByDefault
-import org.kotools.samples.internal.SampleSourceFile
+import org.kotools.samples.internal.isClassDeclaration
+import org.kotools.samples.internal.isJava
+import org.kotools.samples.internal.isKotlin
+import org.kotools.samples.internal.isPublicClassDeclarationInJava
+import org.kotools.samples.internal.isPublicClassDeclarationInKotlin
+import org.kotools.samples.internal.isSample
+import java.io.File
 
 /**
  * Represents a Gradle task that checks the content of the sample source files
@@ -28,8 +34,34 @@ public abstract class CheckSampleSources : DefaultTask() {
     public abstract val sourceDirectory: DirectoryProperty
 
     @TaskAction
-    internal fun execute(): Unit = this.sourceDirectory.asFileTree.asSequence()
-        .filterNotNull()
-        .mapNotNull(SampleSourceFile.Companion::orNull)
-        .forEach(SampleSourceFile::checkSinglePublicClass)
+    internal fun execute() {
+        val noErrorFound: Boolean = this.sourceDirectory.asFileTree.asSequence()
+            .filterNotNull()
+            .filter(File::isSample)
+            .filter { it.isKotlin() || it.isJava() }
+            .toSet()
+            .map { it.hasSinglePublicClass(logger::error) }
+            .all { it }
+        if (noErrorFound) return
+        error("Found some errors while checking sample source files.")
+    }
+}
+
+private fun File.hasSinglePublicClass(log: (String) -> Unit): Boolean {
+    val classDeclarations: List<String> = this.useLines {
+        it.map(String::trim)
+            .filter(String::isClassDeclaration)
+            .toList()
+    }
+    if (classDeclarations.count() > 1) {
+        log("Multiple classes found in '$this'.")
+        return false
+    }
+    val publicClassCount: Int = classDeclarations.count {
+        if (this.isKotlin()) it.isPublicClassDeclarationInKotlin()
+        else it.isPublicClassDeclarationInJava()
+    }
+    if (publicClassCount == 1) return true
+    log("No public class found in '$this'.")
+    return false
 }
