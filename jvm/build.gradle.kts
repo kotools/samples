9ede742a1b5ff79342a813e493d921c826d9f1b5
Loïc Lamarque <loiclamarque777@gmail.com>
2025-06-13 08:29:54 +0200

    ➖ Remove `kotlinx-coroutines-core` dependency from `jvm` project
    This reverts commit 8fb011f08ad6f8bdb67dd4c5db6ab71cf66fe22a.

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -44,7 +44,6 @@ signing {
 dependencies {
     implementation(platform(libs.kotlin.bom))
     implementation(libs.kotlin.gradle.plugin)
-    implementation(libs.kotlin.coroutines.core)
     implementation(libs.dokka.gradle.plugin)
 
     testImplementation(libs.kotlin.test.junit5)
