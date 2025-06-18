15745fc39210bae348ff06d865ec2593930d49fe
Loïc Lamarque <loiclamarque777@gmail.com>
2025-06-18 22:58:16 +0200

    ✨ Add `JavaSampleSource.Companion.orThrow(File)` method

--- jvm/src/main/kotlin/org/kotools/samples/internal/JavaSampleSource.kt
+++ jvm/src/main/kotlin/org/kotools/samples/internal/JavaSampleSource.kt
@@ -15,10 +15,35 @@ internal class JavaSampleSource private constructor() {
          * Returns a Java sample source from the specified [file], or returns
          * `null` if the [file]'s name is not suffixed by `Sample`, or if the
          * [file] has another extension than `java`.
+         *
+         * See the [orThrow] method for throwing an exception instead of
+         * returning `null` in case of invalid [file].
          */
         fun orNull(file: File): JavaSampleSource? = file
             .takeIf { it.nameWithoutExtension.endsWith("Sample") }
             ?.takeIf { it.extension == "java" }
             ?.let { JavaSampleSource() }
+
+        /**
+         * Returns a Java sample source from the specified [file], or throws an
+         * [IllegalArgumentException] if the [file]'s name is not suffixed by
+         * `Sample`, or if the [file] has another extension than `java`.
+         *
+         * See the [orNull] method for returning `null` instead of throwing an
+         * exception in case of invalid [file].
+         */
+        fun orThrow(file: File): JavaSampleSource {
+            val fileName: String = file.nameWithoutExtension
+            require(fileName.endsWith("Sample")) {
+                "Java sample source file name must end with 'Sample' (input: " +
+                        "'$fileName')."
+            }
+            val fileExtension: String = file.extension
+            require(fileExtension == "java") {
+                "Java sample source file extension must be 'java' (input: " +
+                        "'$fileExtension')."
+            }
+            return JavaSampleSource()
+        }
     }
 }
--- jvm/src/test/kotlin/org/kotools/samples/internal/JavaSampleSourceTest.kt
+++ jvm/src/test/kotlin/org/kotools/samples/internal/JavaSampleSourceTest.kt
@@ -2,6 +2,8 @@ package org.kotools.samples.internal
 
 import java.io.File
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
@@ -28,5 +30,36 @@ class JavaSampleSourceTest {
         val file = File("Sample.kt")
         val actual: JavaSampleSource? = JavaSampleSource.orNull(file)
         assertNull(actual)
+    }
+
+    // ------------------------ Companion.orThrow(File) ------------------------
+
+    @Test
+    fun `orThrow passes with Java file having name suffixed by 'Sample'`() {
+        val file = File("Sample.java")
+        JavaSampleSource.orThrow(file)
+    }
+
+    @Test
+    fun `orThrow fails without 'Sample' suffix in Java file's name`() {
+        val file = File("Test.java")
+        val exception: IllegalArgumentException =
+            assertFailsWith { JavaSampleSource.orThrow(file) }
+        val actual: String? = exception.message
+        val expected = "Java sample source file name must end with 'Sample' " +
+                "(input: '${file.nameWithoutExtension}')."
+        assertEquals(expected, actual)
+    }
+
+    @Suppress("SpellCheckingInspection")
+    @Test
+    fun `orThrow fails with file's extension other than 'java'`() {
+        val file = File("Sample.kt")
+        val exception: IllegalArgumentException =
+            assertFailsWith { JavaSampleSource.orThrow(file) }
+        val actual: String? = exception.message
+        val expected = "Java sample source file extension must be 'java' " +
+                "(input: '${file.extension}')."
+        assertEquals(expected, actual)
     }
 }
