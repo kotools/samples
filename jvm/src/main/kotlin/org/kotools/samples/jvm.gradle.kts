a74612f5c875adf472fcbe821b71ef13f211fc29
Loïc Lamarque <loiclamarque777@gmail.com>
2025-07-17 08:43:28 +0200

    🔧 Update configuration of `checkSampleReferences` Gradle task

--- jvm/src/main/kotlin/org/kotools/samples/jvm.gradle.kts
+++ jvm/src/main/kotlin/org/kotools/samples/jvm.gradle.kts
@@ -34,7 +34,7 @@ private val extractSamples: TaskProvider<ExtractSamples> by tasks.registering(
 private val checkSampleReferences: TaskProvider<CheckSampleReferences> by tasks
     .registering(CheckSampleReferences::class) {
         this.description = "Checks sample references from KDoc."
-        this.sourceDirectory = projectSources
+        this.sourceDirectory = projectSources.dir("main")
         this.extractedSamplesDirectory =
             extractSamples.flatMap(ExtractSamples::outputDirectory)
     }
