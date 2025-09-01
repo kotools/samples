f57306081b989197797ffb6b20f024965fe6b532
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-01 18:48:12 +0200

    ✨ Add `JavaSampleSource.Companion.orThrow(File)` method

--- jvm/src/main/kotlin/org/kotools/samples/internal/JavaSampleSource.kt
+++ jvm/src/main/kotlin/org/kotools/samples/internal/JavaSampleSource.kt
@@ -26,4 +26,24 @@ internal value class JavaSampleSource(
     }
 
     override fun toString(): String = "'${this.file}' Java sample source"
+
+    /** Contains static declarations for the [JavaSampleSource] type. */
+    companion object {
+        /**
+         * Returns a Java sample source with the specified [file], or throws an
+         * [IllegalArgumentException] if the [file] has another extension than
+         * `java` or a name that is not suffixed by `Sample`.
+         */
+        fun orThrow(file: File): JavaSampleSource {
+            require(file.extension == "java") {
+                "Java sample source must have 'java' file extension (input: " +
+                        "$file)."
+            }
+            require(file.nameWithoutExtension.endsWith("Sample")) {
+                "Java sample source must have 'Sample' suffix in its file " +
+                        "name (input: $file)."
+            }
+            return JavaSampleSource(file)
+        }
+    }
 }
--- jvm/src/test/kotlin/org/kotools/samples/internal/JavaSampleSourceTest.kt
+++ jvm/src/test/kotlin/org/kotools/samples/internal/JavaSampleSourceTest.kt
@@ -44,4 +44,35 @@ class JavaSampleSourceTest {
             .toString()
         assertEquals(expected = "'$file' Java sample source", actual)
     }
+
+    // ------------------------ Companion.orThrow(File) ------------------------
+
+    @Test
+    fun `orThrow passes with 'Sample' suffix in Java file's name`() {
+        val file = File("Sample.java")
+        val source: JavaSampleSource = JavaSampleSource.orThrow(file)
+        assertEquals(expected = file, actual = source.file)
+    }
+
+    @Test
+    fun `orThrow fails with file having another extension than 'java'`() {
+        val file = File("Sample.txt")
+        val exception: IllegalArgumentException = assertFailsWith {
+            JavaSampleSource.orThrow(file)
+        }
+        val expected =
+            "Java sample source must have 'java' file extension (input: $file)."
+        assertEquals(expected, actual = exception.message)
+    }
+
+    @Test
+    fun `orThrow fails without 'Sample' suffix in file's name`() {
+        val file = File("Test.java")
+        val exception: IllegalArgumentException = assertFailsWith {
+            JavaSampleSource.orThrow(file)
+        }
+        val expected = "Java sample source must have 'Sample' suffix in its " +
+                "file name (input: $file)."
+        assertEquals(expected, actual = exception.message)
+    }
 }
