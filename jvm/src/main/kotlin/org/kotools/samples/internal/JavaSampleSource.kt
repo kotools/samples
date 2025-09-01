f6146ab164f845bb1a908b9d493396c76f83060b
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-01 18:51:17 +0200

    ♻️ Make `JavaSampleSource(File)` constructor private

--- jvm/src/main/kotlin/org/kotools/samples/internal/JavaSampleSource.kt
+++ jvm/src/main/kotlin/org/kotools/samples/internal/JavaSampleSource.kt
@@ -2,29 +2,12 @@ package org.kotools.samples.internal
 
 import java.io.File
 
-/**
- * Represents a sample source written in [Java](https://www.java.com).
- *
- * @constructor Returns a Java sample source with the specified [file], or
- * throws an [IllegalArgumentException] if the [file] has another extension than
- * `java` or a name that is not suffixed by `Sample`.
- */
+/** Represents a sample source written in [Java](https://www.java.com). */
 @JvmInline
-internal value class JavaSampleSource(
+internal value class JavaSampleSource private constructor(
     /** The file of this Java sample source. */
     val file: File
 ) {
-    init {
-        require(this.file.extension == "java") {
-            "Java sample source must have 'java' file extension (input: " +
-                    "${this.file})."
-        }
-        require(this.file.nameWithoutExtension.endsWith("Sample")) {
-            "Java sample source must have 'Sample' suffix in its file name " +
-                    "(input: $file)."
-        }
-    }
-
     override fun toString(): String = "'${this.file}' Java sample source"
 
     /** Contains static declarations for the [JavaSampleSource] type. */
--- jvm/src/test/kotlin/org/kotools/samples/internal/JavaSampleSourceTest.kt
+++ jvm/src/test/kotlin/org/kotools/samples/internal/JavaSampleSourceTest.kt
@@ -6,41 +6,12 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 class JavaSampleSourceTest {
-    // --------------------------- constructor(File) ---------------------------
-
-    @Test
-    fun `constructor passes with 'Sample' suffix in Java file's name`() {
-        val file = File("Sample.java")
-        val source = JavaSampleSource(file)
-        assertEquals(expected = file, actual = source.file)
-    }
-
-    @Test
-    fun `constructor fails with file having another extension than 'java'`() {
-        val file = File("Sample.txt")
-        val exception: IllegalArgumentException =
-            assertFailsWith { JavaSampleSource(file) }
-        val expected =
-            "Java sample source must have 'java' file extension (input: $file)."
-        assertEquals(expected, actual = exception.message)
-    }
-
-    @Test
-    fun `constructor fails without 'Sample' suffix in file's name`() {
-        val file = File("Test.java")
-        val exception: IllegalArgumentException =
-            assertFailsWith { JavaSampleSource(file) }
-        val expected = "Java sample source must have 'Sample' suffix in its " +
-                "file name (input: $file)."
-        assertEquals(expected, actual = exception.message)
-    }
-
     // ------------------------------ toString() -------------------------------
 
     @Test
     fun `toString passes`() {
         val file = File("Sample.java")
-        val actual: String = JavaSampleSource(file)
+        val actual: String = JavaSampleSource.orThrow(file)
             .toString()
         assertEquals(expected = "'$file' Java sample source", actual)
     }
