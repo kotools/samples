9cb3170a84243ce1c078abe83120621a86792199
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-01 17:46:09 +0200

    ✨ Add `Error.Companion.orThrow(String)` method

--- jvm/src/main/kotlin/org/kotools/samples/internal/Error.kt
+++ jvm/src/main/kotlin/org/kotools/samples/internal/Error.kt
@@ -17,4 +17,17 @@ internal value class Error(
     }
 
     override fun toString(): String = this.message
+
+    /** Contains static declarations for the [Error] type. */
+    companion object {
+        /**
+         * Returns an error with the specified [message], or throws an
+         * [IllegalArgumentException] if the [message] is
+         * [blank][String.isBlank].
+         */
+        fun orThrow(message: String): Error {
+            require(message.isNotBlank()) { "Blank error's message specified." }
+            return Error(message)
+        }
+    }
 }
--- jvm/src/test/kotlin/org/kotools/samples/internal/ErrorTest.kt
+++ jvm/src/test/kotlin/org/kotools/samples/internal/ErrorTest.kt
@@ -29,4 +29,22 @@ class ErrorTest {
         val actual: String = error.toString()
         assertEquals(expected = error.message, actual)
     }
+
+    // ----------------------- Companion.orThrow(String) -----------------------
+
+    @Test
+    fun `orThrow passes with non-blank message`() {
+        val message = "Error found."
+        val error: Error = Error.orThrow(message)
+        assertEquals(expected = message, actual = error.message)
+    }
+
+    @Test
+    fun `orThrow fails with blank message`() {
+        val exception: IllegalArgumentException = assertFailsWith {
+            Error.orThrow(" ")
+        }
+        val expected = "Blank error's message specified."
+        assertEquals(expected, actual = exception.message)
+    }
 }
