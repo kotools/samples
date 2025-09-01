57667c06456fa85a28ceed88855ceab122fb7845
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-01 17:55:23 +0200

    ✨ Add `Error.Companion.orNull(String)` method

--- jvm/src/main/kotlin/org/kotools/samples/internal/Error.kt
+++ jvm/src/main/kotlin/org/kotools/samples/internal/Error.kt
@@ -21,6 +21,13 @@ internal value class Error(
     /** Contains static declarations for the [Error] type. */
     companion object {
         /**
+         * Returns an error with the specified [message], or returns `null` if
+         * the [message] is [blank][String.isBlank].
+         */
+        fun orNull(message: String): Error? = message.takeIf(String::isNotBlank)
+            ?.let(::Error)
+
+        /**
          * Returns an error with the specified [message], or throws an
          * [IllegalArgumentException] if the [message] is
          * [blank][String.isBlank].
--- jvm/src/test/kotlin/org/kotools/samples/internal/ErrorTest.kt
+++ jvm/src/test/kotlin/org/kotools/samples/internal/ErrorTest.kt
@@ -3,6 +3,7 @@ package org.kotools.samples.internal
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertNull
 
 class ErrorTest {
     // -------------------------- constructor(String) --------------------------
@@ -28,6 +29,21 @@ class ErrorTest {
         val error = Error("Error found.")
         val actual: String = error.toString()
         assertEquals(expected = error.message, actual)
+    }
+
+    // ----------------------- Companion.orNull(String) ------------------------
+
+    @Test
+    fun `orNull passes with non-blank message`() {
+        val message = "Error found."
+        val error: Error? = Error.orNull(message)
+        assertEquals(expected = message, actual = error?.message)
+    }
+
+    @Test
+    fun `orNull fails with blank message`() {
+        val error: Error? = Error.orNull(" ")
+        assertNull(error)
     }
 
     // ----------------------- Companion.orThrow(String) -----------------------
