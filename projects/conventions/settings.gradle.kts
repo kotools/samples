c96b6498e17c8b13f7ac50fac664c3e44dae67a0
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-18 23:26:05 +0100

    👷 Update settings of `:conventions` project (#42)

--- projects/conventions/settings.gradle.kts
+++ projects/conventions/settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "conventions"
+
+dependencyResolutionManagement.versionCatalogs.register("libs") {
+    gradle.parent?.rootProject?.layout?.projectDirectory
+        ?.files("gradle/libs.versions.toml")
+        ?.let(this::from)
+}
+
+gradle.startParameter.isDryRun = gradle.parent?.startParameter?.isDryRun == true
