b92f273427d9140ca205261004d4115154fbb8cc
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-18 23:13:24 +0100

    🎉 Add `:conventions` Gradle project (#42)

---
+++ projects/conventions/README.md
@@ -0,0 +1,3 @@
+# `conventions` included build
+
+This build provides Gradle conventions plugins for Kotools Samples.
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,6 @@ rootProject.name = "samples"
 include("samples")
 project(":samples").projectDir = rootDir.resolve("projects/plugin")
 
+includeBuild("projects/conventions")
 includeBuild("jvm")
 includeBuild("jvm-demo")
