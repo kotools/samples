e623ba512827e5eb17f297d137e58590c4335f22
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-27 21:25:54 +0100

    🚧 Add `org.kotools.samples` plugin in `samples-gradle-plugin` (#42)

--- projects/gradle-plugin/build.gradle.kts
+++ projects/gradle-plugin/build.gradle.kts
@@ -18,3 +18,14 @@ compatibility {
     this.java.set(libs.versions.java)
     this.kotlin.set(libs.versions.kotlin)
 }
+
+gradlePlugin {
+    this.vcsUrl.set("https://github.com/kotools/samples")
+    this.plugins.register("org.kotools.samples").configure {
+        this.displayName = "Kotools Samples"
+        this.description =
+            "Plugin that inlines read-only Kotlin samples into documentation."
+        this.id = this.name
+        this.implementationClass = "${this.name}.gradle.KotoolsSamplesPlugin"
+    }
+}
