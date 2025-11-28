08ed308adfdfa8f496cfd5f6cc878cf1164f1783
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-28 14:47:53 +0100

    ➕ Add `kotlin-dsl` plugin dependency to `conventions` (#42)

---
+++ projects/conventions/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins { this.`kotlin-dsl` }
+
+repositories.mavenCentral()
+
+kotlin {
+    this.explicitApi()
+    this.compilerOptions.allWarningsAsErrors.set(true)
+}
+
+tasks {
+    this.withType<ValidatePlugins>().configureEach {
+        this.failOnWarning.set(true)
+        this.enableStricterValidation.set(true)
+    }
+
+    this.test.configure(Test::useJUnitPlatform)
+
+    val moduleTaskGroup = "module"
+    this.tasks.configure { this.displayGroup = moduleTaskGroup }
+    this.assemble.configure { this.group = moduleTaskGroup }
+    this.check.configure { this.group = moduleTaskGroup }
+    this.build.configure { this.group = moduleTaskGroup }
+}
