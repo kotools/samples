22c85fb19fa3c7a6b1c8fc42977ab42ccc302909
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-28 19:24:21 +0100

    💚 Fix ABI of `CompatibilityPlugin` type (#42)

--- projects/conventions/src/main/kotlin/org/kotools/samples/conventions/CompatibilityPlugin.kt
+++ projects/conventions/src/main/kotlin/org/kotools/samples/conventions/CompatibilityPlugin.kt
@@ -6,7 +6,7 @@ import org.gradle.api.Project
 /**
  * Plugin that configures the compatibility of a project with its dependencies.
  */
-public class CompatibilityPlugin : Plugin<Project> {
+public class CompatibilityPlugin internal constructor() : Plugin<Project> {
     /** Applies this plugin to the specified [project]. */
     override fun apply(project: Project): Unit = Unit
 }
