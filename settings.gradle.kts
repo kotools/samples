66d3e4c0164c16f36cb60354390e7532292befb5
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-28 14:14:12 +0100

    🎉 Add `conventions`, `demonstration` and `samples-gradle-plugin` projects (#42)
    These projects are empty for now.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
 rootProject.name = "samples"
 
+private fun subproject(name: String, directoryName: String = name) {
+    include(name)
+    project(":$name").projectDir = rootDir.resolve("projects/$directoryName")
+}
+
+subproject("conventions")
+subproject("demonstration")
+subproject(name = "samples-gradle-plugin", directoryName = "plugin")
+
 includeBuild("jvm")
 includeBuild("jvm-demo")
