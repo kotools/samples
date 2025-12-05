b38bfb29cfbfd4777ccfae4d1d7db175aae270c2
Loïc Lamarque <loiclamarque777@gmail.com>
2025-12-05 17:23:59 +0100

    ➕ Add `kotlin-jvm` plugin dependency to `demonstration` project (#42)

---
+++ projects/demonstration/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins { this.alias(libs.plugins.kotlin.jvm) }
+
+repositories.mavenCentral()
+
+java {
+    val version: JavaVersion = JavaVersion.VERSION_17
+    this.sourceCompatibility = version
+    this.targetCompatibility = version
+}
+
+kotlin {
+    this.explicitApi()
+    this.compilerOptions {
+        this.allWarningsAsErrors.set(true)
+        this.apiVersion.set(KotlinVersion.KOTLIN_2_0)
+        this.languageVersion.set(KotlinVersion.KOTLIN_1_8)
+        this.jvmTarget.set(JvmTarget.JVM_17)
+    }
+    this.coreLibrariesVersion = libs.versions.kotlin.get()
+}
+
+tasks {
+    this.test.configure(Test::useJUnitPlatform)
+
+    val moduleTaskGroup = "module"
+    this.tasks.configure { this.displayGroup = moduleTaskGroup }
+    this.assemble.configure { this.group = moduleTaskGroup }
+    this.check.configure { this.group = moduleTaskGroup }
+    this.build.configure { this.group = moduleTaskGroup }
+}
