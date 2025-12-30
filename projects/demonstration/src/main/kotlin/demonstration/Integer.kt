94a539dedf12dc213609241cd53af1154c5d0cff
Loïc Lamarque <loiclamarque777@gmail.com>
2025-12-30 09:50:53 +0100

    ✨ Add main functions in `demonstration` (#42)

---
+++ projects/demonstration/src/main/kotlin/demonstration/Integer.kt
@@ -0,0 +1,29 @@
+package demonstration
+
+/**
+ * Performs an addition with [x] and [y] integers (`x + y`).
+ *
+ * SAMPLE: [demonstration.IntSample.addition]
+ */
+public fun addition(x: Int, y: Int): Int = x + y
+
+/**
+ * Performs an addition with [x] and [y] integers (`x + y`).
+ *
+ * SAMPLE: [demonstration.LongSample.addition]
+ */
+public fun addition(x: Long, y: Long): Long = x + y
+
+/**
+ * Performs a subtraction with [x] and [y] integers (`x - y`).
+ *
+ * SAMPLE: [demonstration.IntSample.subtraction]
+ */
+public fun subtraction(x: Int, y: Int): Int = x - y
+
+/**
+ * Performs a subtraction with [x] and [y] integers (`x - y`).
+ *
+ * SAMPLE: [demonstration.LongSample.subtraction]
+ */
+public fun subtraction(x: Long, y: Long): Long = x - y
