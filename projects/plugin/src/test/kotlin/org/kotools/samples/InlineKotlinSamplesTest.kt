98d34f7e56b6acc93edf9747c87a0c1d1033e608
Loïc Lamarque <loiclamarque777@gmail.com>
2026-01-07 20:23:00 +0100

    ✅ Test Kotlin samples inlining into multi-line KDoc (#42)

--- projects/plugin/src/test/kotlin/org/kotools/samples/InlineKotlinSamplesTest.kt
+++ projects/plugin/src/test/kotlin/org/kotools/samples/InlineKotlinSamplesTest.kt
@@ -36,7 +36,42 @@ class InlineKotlinSamplesTest {
             """.trimIndent()
         )
     }
-    // TODO: inlines sample into multi-line KDoc
+
+    @Test
+    fun `inlines sample into multi-line KDoc`() {
+        // Given
+        val project: GradleProject = GradleProject.create()
+        project.sampleSource(
+            """
+                class IntSample {
+                    fun addition(): Unit = check(1 + 2 == 3)
+                }
+            """.trimIndent()
+        )
+        project.mainSource(
+            """
+                /**
+                 * SAMPLE: [IntSample.addition]
+                 */
+                fun addition(x: Int, y: Int): Int = x + y
+            """.trimIndent()
+        )
+
+        // When
+        project.successfulBuild(taskPath = ":inlineKotlinSamples")
+
+        // Then
+        project.assertInlinedMainSource(
+            """
+                /**
+                 * ```kotlin
+                 * check(1 + 2 == 3)
+                 * ```
+                 */
+                fun addition(x: Int, y: Int): Int = x + y
+            """.trimIndent()
+        )
+    }
     // TODO: inlines multiple samples into KDoc
     // TODO: fails when referenced sample doesn't exist
 }
