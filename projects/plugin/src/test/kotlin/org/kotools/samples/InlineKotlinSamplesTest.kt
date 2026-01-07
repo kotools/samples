5da05bc500f630c9c5fd1c85c1502865a3f7dbb8
Loïc Lamarque <loiclamarque777@gmail.com>
2026-01-07 20:34:28 +0100

    ✅ Test Kotlin samples inlining with multiple samples into KDoc (#42)

--- projects/plugin/src/test/kotlin/org/kotools/samples/InlineKotlinSamplesTest.kt
+++ projects/plugin/src/test/kotlin/org/kotools/samples/InlineKotlinSamplesTest.kt
@@ -3,6 +3,8 @@ package org.kotools.samples
 import kotlin.test.Test
 
 class InlineKotlinSamplesTest {
+    private val taskPath: String = ":inlineKotlinSamples"
+
     @Test
     fun `inlines sample into single-line KDoc`() {
         // Given
@@ -25,7 +27,7 @@ class InlineKotlinSamplesTest {
         )
 
         // When
-        project.successfulBuild(taskPath = ":inlineKotlinSamples")
+        project.successfulBuild(this.taskPath)
 
         // Then
         project.assertInlinedMainSource(
@@ -65,7 +67,7 @@ class InlineKotlinSamplesTest {
         )
 
         // When
-        project.successfulBuild(taskPath = ":inlineKotlinSamples")
+        project.successfulBuild(this.taskPath)
 
         // Then
         project.assertInlinedMainSource(
@@ -80,6 +82,59 @@ class InlineKotlinSamplesTest {
             """.trimIndent()
         )
     }
-    // TODO: inlines multiple samples into KDoc
+
+    @Test
+    fun `inlines multiple samples into KDoc`() {
+        // Given
+        val project: GradleProject = GradleProject.create()
+        project.sampleSource(
+            """
+                class IntSample {
+                    fun addition() {
+                        val result: Int = addition(x = 1, y = 2)
+                        check(result == 3)
+                    }
+
+                    fun subtraction() {
+                        val result: Int = subtraction(x = 3, y = 2)
+                        check(result == 1)
+                    }
+                }
+            """.trimIndent()
+        )
+        project.mainSource(
+            """
+                /** SAMPLE: [IntSample.addition] */
+                fun addition(x: Int, y: Int): Int = x + y
+
+                /** SAMPLE: [IntSample.subtraction] */
+                fun subtraction(x: Int, y: Int): Int = x - y
+            """.trimIndent()
+        )
+
+        // When
+        project.successfulBuild(this.taskPath)
+
+        // Then
+        project.assertInlinedMainSource(
+            """
+                /**
+                 * ```kotlin
+                 * val result: Int = addition(x = 1, y = 2)
+                 * check(result == 3)
+                 * ```
+                 */
+                fun addition(x: Int, y: Int): Int = x + y
+
+                /**
+                 * ```kotlin
+                 * val result: Int = subtraction(x = 3, y = 2)
+                 * check(result == 1)
+                 * ```
+                 */
+                fun subtraction(x: Int, y: Int): Int = x - y
+            """.trimIndent()
+        )
+    }
     // TODO: fails when referenced sample doesn't exist
 }
